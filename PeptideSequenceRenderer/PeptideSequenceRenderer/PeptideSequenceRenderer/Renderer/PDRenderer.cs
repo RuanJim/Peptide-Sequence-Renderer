3e9839ad4f6faee6424bf4bda66b59a43731397f
Jim <jim.ruan@perkinelmer.com>
2018-06-14 14:28:17 +0800

    TG-39 #closed.

--- PeptideSequenceRenderer/PeptideSequenceRenderer/PeptideSequenceRenderer/Renderer/PDRenderer.cs
+++ PeptideSequenceRenderer/PeptideSequenceRenderer/PeptideSequenceRenderer/Renderer/PDRenderer.cs
@@ -13,6 +13,11 @@
 
 #region
 
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text.RegularExpressions;
 using Spotfire.Dxp.Application.Extension;
 using Spotfire.Dxp.Application.Visuals.ValueRenderers;
 
@@ -22,9 +27,49 @@ namespace Com.PerkinElmer.Service.PeptideSequenceRenderer.Renderer
 {
     public sealed class PDRenderer : CustomValueRenderer
     {
-        protected override void RenderCore(ValueRendererSettings rendererSettings, ValueRendererArgs args, ValueRendererResult renderingResult)
+        protected override void RenderCore(ValueRendererSettings rendererSettings, ValueRendererArgs rendererArgs, ValueRendererResult renderingResult)
         {
-            renderingResult.SetImage(System.Drawing.Image.FromFile(@"H:\1.png"));
+            Regex regex = new Regex(@"PEPTIDE1\{\[.*?_(.*?)\]\.(.*?)\}");
+
+            var match = regex.Match(rendererArgs.DataValue.ValidValue.ToString());
+
+            List<string> peptideList = new List<string>();
+
+            peptideList.Add(match.Groups[1].Captures[0].Value);
+
+            peptideList.AddRange(match.Groups[2].Captures[0].Value.Split(new char[] { '.' }).ToArray());
+
+
+            Bitmap bitmap = new Bitmap(rendererArgs.Width, rendererArgs.Height);
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+
+                for (int i = 0; i < peptideList.Count; i++)
+                {
+                    string monomer = peptideList[i].Replace("[", string.Empty).Replace("]", string.Empty);
+
+                    var color = PDRenderAddin.MonomerColorTable[monomer];
+
+                    g.DrawRectangle(new Pen(Color.White), i * 20, 0, 20, 20);
+                    g.FillRectangle(new SolidBrush(ColorTranslator.FromHtml(color.BackgroundColor)), i * 20 + 1, 1, 18, 18);
+
+                    Font GraphicFont = new Font("Courier New", 10);
+
+                    Point point = new Point(i * 20, 0);
+                    StringFormat sf = new StringFormat();
+
+                    sf.Alignment = StringAlignment.Near;
+                    sf.LineAlignment = StringAlignment.Near;
+
+                    g.DrawString(monomer, GraphicFont, new SolidBrush(ColorTranslator.FromHtml(color.ForeColor)), point, sf);
+                }
+
+            }
+
+            renderingResult.SetImage(bitmap);
+            renderingResult.SetTooltip(rendererArgs.DataValue.ValidValue.ToString());
+
         }
     }
 }
