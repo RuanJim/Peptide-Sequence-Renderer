b79c3e645d17111a2004ad6283cd017ef9d78a00
Jim <jim.ruan@perkinelmer.com>
2018-06-20 15:07:24 +0800

    TG-39, center monomer.

--- PeptideSequenceRenderer/PeptideSequenceRenderer/PeptideSequenceRenderer/Renderer/PDRenderer.cs
+++ PeptideSequenceRenderer/PeptideSequenceRenderer/PeptideSequenceRenderer/Renderer/PDRenderer.cs
@@ -10,14 +10,15 @@
 //     that authorizes such use.
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
-
 #region
 
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Com.PerkinElmer.Service.PeptideSequenceRenderer.Models;
 using Spotfire.Dxp.Application.Extension;
 using Spotfire.Dxp.Application.Visuals.ValueRenderers;
 
@@ -29,6 +30,10 @@ namespace Com.PerkinElmer.Service.PeptideSequenceRenderer.Renderer
     {
         protected override void RenderCore(ValueRendererSettings rendererSettings, ValueRendererArgs rendererArgs, ValueRendererResult renderingResult)
         {
+            int cellSize = rendererArgs.Height;
+
+            PDRenderSettings settings = (PDRenderSettings) rendererSettings;
+
             Regex regex = new Regex(@"PEPTIDE1\{\[.*?_(.*?)\]\.(.*?)\}");
 
             var match = regex.Match(rendererArgs.DataValue.ValidValue.ToString());
@@ -39,8 +44,8 @@ namespace Com.PerkinElmer.Service.PeptideSequenceRenderer.Renderer
 
             peptideList.AddRange(match.Groups[2].Captures[0].Value.Split(new char[] { '.' }).ToArray());
 
-
             Bitmap bitmap = new Bitmap(rendererArgs.Width, rendererArgs.Height);
+
             using (Graphics g = Graphics.FromImage(bitmap))
             {
                 g.SmoothingMode = SmoothingMode.AntiAlias;
@@ -51,18 +56,27 @@ namespace Com.PerkinElmer.Service.PeptideSequenceRenderer.Renderer
 
                     var color = PDRenderAddin.MonomerColorTable[monomer];
 
-                    g.DrawRectangle(new Pen(Color.White), i * 20, 0, 20, 20);
-                    g.FillRectangle(new SolidBrush(ColorTranslator.FromHtml(color.BackgroundColor)), i * 20 + 1, 1, 18, 18);
+                    Rectangle rect = new Rectangle(i * cellSize, 0, cellSize, cellSize);
 
-                    Font GraphicFont = new Font("Courier New", 10);
+                    g.FillRectangle(new SolidBrush(ColorTranslator.FromHtml(color.BackgroundColor)), rect);
+                    g.DrawRectangle(new Pen(Color.White), rect);
 
-                    Point point = new Point(i * 20, 0);
+                    FontFamily fontFamily = new FontFamily("Tahoma");
+
+                    fontFamily.GetEmHeight(FontStyle.Regular);
+                    float ascent = fontFamily.GetCellAscent(FontStyle.Regular);
+                    float descent = fontFamily.GetCellDescent(FontStyle.Regular);
+
+                    float size = Convert.ToSingle(cellSize) / Convert.ToSingle(monomer.Length) * 0.5f * fontFamily.GetEmHeight(FontStyle.Regular) / (ascent + descent);
+
+                    Font font = new Font(fontFamily, size, FontStyle.Regular);
+
                     StringFormat sf = new StringFormat();
 
-                    sf.Alignment = StringAlignment.Near;
-                    sf.LineAlignment = StringAlignment.Near;
+                    sf.Alignment = StringAlignment.Center;
+                    sf.LineAlignment = StringAlignment.Center;
 
-                    g.DrawString(monomer, GraphicFont, new SolidBrush(ColorTranslator.FromHtml(color.ForeColor)), point, sf);
+                    g.DrawString(monomer, font, new SolidBrush(ColorTranslator.FromHtml(color.ForeColor)), rect, sf);
                 }
 
             }
