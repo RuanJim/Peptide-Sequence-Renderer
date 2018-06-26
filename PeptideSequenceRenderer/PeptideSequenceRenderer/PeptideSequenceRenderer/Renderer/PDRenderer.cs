a46360aa2586e2617950383f269dc6450ea91433
Jim <jim.ruan@perkinelmer.com>
2018-06-26 16:01:48 +0800

    TG-50 #closed.

--- PeptideSequenceRenderer/PeptideSequenceRenderer/PeptideSequenceRenderer/Renderer/PDRenderer.cs
+++ PeptideSequenceRenderer/PeptideSequenceRenderer/PeptideSequenceRenderer/Renderer/PDRenderer.cs
@@ -39,10 +39,28 @@ namespace Com.PerkinElmer.Service.PeptideSequenceRenderer.Renderer
             var match = regex.Match(rendererArgs.DataValue.ValidValue.ToString());
 
             List<string> peptideList = new List<string>();
+            List<char> linkerList = new List<char>();
+
+
+            // TODO: Change spliter to "\n"
+            string linkerString = rendererArgs.DataValue.ValidValue.ToString()
+                .Split(new string[] {"$$$"}, StringSplitOptions.None)[1];
+
+            linkerList.AddRange(linkerString.ToCharArray());
 
             peptideList.Add(match.Groups[1].Captures[0].Value);
 
             peptideList.AddRange(match.Groups[2].Captures[0].Value.Split(new char[] { '.' }).ToArray());
+
+            int startIndex = peptideList.Count - linkerList.Count;
+
+            for (var i = peptideList.Count - 1; i >= startIndex; i--)
+            {
+                if (peptideList[i] == linkerList[i - startIndex].ToString())
+                {
+                    peptideList[i] = $"#{peptideList[i]}#";
+                }
+            }
 
             Bitmap bitmap = new Bitmap(rendererArgs.Width, rendererArgs.Height);
 
@@ -74,7 +92,17 @@ namespace Com.PerkinElmer.Service.PeptideSequenceRenderer.Renderer
                         };
                     }
 
+                    if (monomer.StartsWith("#") && monomer.EndsWith("#"))
+                    {
+                        color = new ColorSetting
+                        {
+                            ForeColor = settings.DefaultFontColor,
+                            BackgroundColor = settings.DefaultBackgroundColor
+                        };
+                    }
+
                     monomer = monomer.Replace("(", string.Empty).Replace(")", string.Empty);
+                    monomer = monomer.Replace("#", string.Empty).Replace("#", string.Empty);
 
                     Rectangle rect = new Rectangle(i * cellSize, 0, cellSize, cellSize);
 
