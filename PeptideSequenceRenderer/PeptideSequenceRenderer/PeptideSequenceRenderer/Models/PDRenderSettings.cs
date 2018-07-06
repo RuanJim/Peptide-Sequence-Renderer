08ae412064baf9d975632015c772a06b75bdf695
Jim <jim.ruan@perkinelmer.com>
2018-07-06 14:30:36 +0800

    Get monomer color table.

--- PeptideSequenceRenderer/PeptideSequenceRenderer/PeptideSequenceRenderer/Models/PDRenderSettings.cs
+++ PeptideSequenceRenderer/PeptideSequenceRenderer/PeptideSequenceRenderer/Models/PDRenderSettings.cs
@@ -56,7 +56,7 @@ namespace Com.PerkinElmer.Service.PeptideSequenceRenderer.Models
             CreateProperty(PropertyNames.DefaultBackgroundColor, out _defaultBackgroundColor, "#FFFFFF");
             CreateProperty(PropertyNames.BranchMonomerFontColor, out _branchMonomerFontColor, "#000000");
             CreateProperty(PropertyNames.BranchMonomerBackgroundColor, out _branchMonomerBackgroundColor, "#FFFFFF");
-            CreateProperty(PropertyNames.ColorCodeTable, out this._colorTable, null);
+            CreateProperty(PropertyNames.ColorCodeTable, out this._colorTable, string.Empty);
         }
 
         internal PDRenderSettings(SerializationInfo info, StreamingContext context) : base(info, context)
@@ -72,7 +72,6 @@ namespace Com.PerkinElmer.Service.PeptideSequenceRenderer.Models
             DeserializeProperty<string>(info, context, PropertyNames.BranchMonomerFontColor, out _branchMonomerFontColor);
             DeserializeProperty<string>(info, context, PropertyNames.BranchMonomerBackgroundColor, out _branchMonomerBackgroundColor);
             DeserializeProperty<string>(info, context, PropertyNames.ColorCodeTable, out _colorTable);
-
         }
 
         public int MaxAcidAmount
